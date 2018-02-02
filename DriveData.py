0ed75f6519d2866b88a7a854b4d183536e9e18f0
JeongGyu Kim (zzqyu) <wjdrb0626@gmail.com>
2018-02-02 17:20:37 +0900

    DriveData class ver.2
    인터넷 미연결시 예외처리 적용

--- DriveData.py
+++ DriveData.py
@@ -12,6 +12,7 @@ class DriveData:
 		self.routeId = routeId
 		self.xmlStr = "" ##xml문자열 담을 변수
 		self.root = None ##최상위 항목 담을 변수
+		self.isOnInternet = True ## 인터넷연결여부 
 		
 		##xml요청 주소에 넘길 인자 세팅
 		queryParams = '?' + urlencode({quote_plus('serviceKey') : "KEYKEY", quote_plus('routeId') : self.routeId })
@@ -20,19 +21,28 @@ class DriveData:
 		##xml문서 받아와 str으로 xmlStr에 담는다.
 		request = Request(self.url + queryParams)
 		request.get_method = lambda: 'GET'
-		self.xmlStr = DriveData.binToUtf8(urlopen(request).read())
-		self.root = fromstring(self.xmlStr)
+		try:
+			self.xmlStr = DriveData.binToUtf8(urlopen(request).read())
+			self.root = fromstring(self.xmlStr)
+		except:
+			self.isOnInternet = False
 		
 	## 본 정보의 루트태그 msgBody의 유무 체크
 	def isSuccess(self):
+		if not self.isOnInternet:
+			return False
 		return self.root.find("msgBody")!=None
 		
 	## xml문서의 루트태그 리턴
 	def getRoot(self):
+		if not self.isOnInternet:
+			return None
 		return self.root
 		
 	## xml문서에서 본 정보(버스위치)리스트 리턴
 	def getBusLocations(self):
+		if not self.isOnInternet:
+			return []
 		return self.root.find("msgBody").getchildren()
 		
 	## binary data to utf-8
