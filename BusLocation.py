56a6d4e8f9d911d3c7875496b7d68c3285ce4fe5
JeongGyu Kim (zzqyu) <wjdrb0626@gmail.com>
2018-02-02 17:11:36 +0900

    BusLocation class ver.1

---
+++ BusLocation.py
@@ -0,0 +1,33 @@
+﻿from DriveData import DriveData
+
+class BusLocation:
+	##조회할 노선 아이디를 받는다.
+	def __init__(self, rootTag):
+		BusLocation.childTagName = ("routeId", "stationId", "endBus", "plateNo")
+		self.root = rootTag
+		self.dataDict = {}
+		for t in BusLocation.childTagName:
+			self.dataDict[t] = self.root.findtext(t)
+		
+		
+		
+	def getRouteId(self):
+		return self.dataDict[BusLocation.childTagName[0]]
+	def getStationId(self):
+		return self.dataDict[BusLocation.childTagName[1]]
+	def getEndBus(self):
+		return self.dataDict[BusLocation.childTagName[2]]
+	def getPlateNo(self):
+		return self.dataDict[BusLocation.childTagName[3]]
+	def getAll(self):
+		return self.dataDict
+		
+		
+##테스트코드
+if __name__ == "__main__" :
+	dd = DriveData("233000010")
+	for bl in dd.getBusLocations():
+		cbl = BusLocation(bl)
+		print(cbl.getAll())
+
+
