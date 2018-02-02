94d39abd54bc24e65bdbff9c6c7626303b909136
JeongGyu Kim (zzqyu) <wjdrb0626@gmail.com>
2018-02-02 19:50:45 +0900

    테스트 프로그램 주석추가

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from RouteStation import RouteStation ##정류장 목록
 import datetime
 import time
 
-
+## 노선번호:노선ID
 routeList = {
 	"1" : "233000048", "1-1": "233000051", "2": "233000013", "2-1": "233000052", 
 	"2-2" : "233000035", "2-3": "233000030", "2-4": "233000029", "3": "233000028", 
@@ -19,25 +19,34 @@ routeList = {
 	"33-2" : "233000126", "50-1": "241483004", "50-1(대광1차)": "241483010", "2000B": "233000264", 
 	"2000A" : "233000263", "8155": "233000139", "9801": "241005320", "9802": "241005300"
 	}
+	
+##노선번호:정류장목록
 routeStationList = {}
 for no in routeList.keys():
 	rs = RouteStation(routeList[no])
 	routeStationList[no]=rs.getStationNames()
-
+##메인 루프
 while True:
+	##현재시간 프린트
 	print(datetime.datetime.now())
+	
+	##모든 노선을 조회하는 루프
 	for no in routeList.keys():
-		
+		##조회할 노선의 버스위치 목록
 		dd = DriveData(routeList[no])
 		if not dd.isConnectInternet():
 			print("인터넷 확인하세요!")
 			break
 		locaList = dd.getBusLocations()
+		
 		if len(locaList)>0:
 			print(no, "번")
+			##조회한 노선의 모든 버스위치를 출력하는 루프 
 			for bl in locaList:
 				cbl = BusLocation(bl)
+				##정류장순서
 				seq = int(cbl.getStationSeq())
+				##버스 및 현재 정류장정보 출력
 				print(cbl.getAll(), end=" ")
 				print(routeStationList[no][seq])
 		
