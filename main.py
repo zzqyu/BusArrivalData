b939a0b24e1e19e38a94dcd631d52f2300e25dc7
JeongGyu Kim (zzqyu) <wjdrb0626@gmail.com>
2018-02-06 21:44:53 +0900

    경유 정류소 수집 안하기

--- main.py
+++ main.py
@@ -91,10 +91,10 @@ while True:##하루에 1번 작동하는 루프(날짜바뀔때)
 						##버스 및 현재 정류장정보 출력
 						locaInfo =  cbl.getAll()
 						nowLocaList.append(locaInfo)
-						if not locaInfo in preLocaList:
+						if (not locaInfo in preLocaList) or (not "(경유)" in stationName) :
 							print("[seq]", seq, "[id]", cbl.getStationId(), end = "" )
-							print("[name]", routeStationList[no][seq])
-							dbc.addData(DBControl.dateToTableName(date), ( str(curCount),   routeStationList[no][seq], cbl.getStationId(), curTime, no, cbl.getRouteId(), cbl.getEndBus(), weekday, str(int(isHoliday))  ) )
+							print("[name]", stationName)
+							dbc.addData(DBControl.dateToTableName(date), ( str(curCount),  stationName,  cbl.getStationId(), curTime, no, cbl.getRouteId(), cbl.getEndBus(), weekday, str(int(isHoliday))  ) )
 							dbc.incRowViaTable(DBControl.dateToTableName(date))
 							curCount+=1	
 			preLocaList = nowLocaList[:]
